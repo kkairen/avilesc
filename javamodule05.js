2b40224063c0db0b0676646cefd10edb75427f15
Coral Aviles <coralaaviles@gmail.com>
2018-05-03 08:59:15 -0700

    Update javamodule05.js

--- javamodule05.js
+++ javamodule05.js
@@ -1,4 +1,4 @@
-var table = prompt("Enter Unit of Table: ", 10);             // Unit of table
+var table = prompt("Enter a value for the table: ", 10);             // Unit of table
 var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';              // Message
@@ -7,10 +7,10 @@ if (operator === 'addition') {
   // Do addition
   while (i < 11) {
     msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++
+    i++;
   }
-  else{
-    // Do multiplication
+} else {
+  // Do multiplication
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
