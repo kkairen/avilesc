9df4c0173bd786d9585d47d8cce6bfad212b3704
Coral Aviles <coralaaviles@gmail.com>
2018-05-03 08:56:20 -0700

    Update javamodule05.js

--- javamodule05.js
+++ javamodule05.js
@@ -3,7 +3,14 @@ var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';              // Message
 
-if (operator === 'multiplication') {
+if (operator === 'addition') {
+  // Do addition
+  while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+    i++
+  }
+  else{
+    // Do multiplication
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
